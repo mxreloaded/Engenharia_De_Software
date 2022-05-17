1009908f38779a00fb08b287a42b30f3bf93262c
pedroara <araujo.peo@gmail.com>
2022-05-16 22:20:12 -0300

    Send user in login

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -6,7 +6,7 @@ class AuthenticationController < ApplicationController
         @user = User.find_by_email(params[:email])
         if @user&.authenticate(params[:password])
             token = jwt_encode(user_id: @user.id)
-            render json: { token: token}, status: :ok
+            render json: { token: token, user: Api::V1::UserSerializer.call(User.find(jwt_decode(token)[:user_id]))}, status: :ok
         else
             render json: { error: 'unauthorized' }, status: :unauthorized
         end
