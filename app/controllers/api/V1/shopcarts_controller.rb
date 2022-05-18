9ddb5f26706d50dafaf35b793473689d1986bd0b
pedroara <araujo.peo@gmail.com>
2022-05-17 23:55:36 -0300

    Quick fix 4

--- app/controllers/api/V1/shopcarts_controller.rb
+++ app/controllers/api/V1/shopcarts_controller.rb
@@ -33,9 +33,8 @@ module Api
         
         name = params[:name] if params[:name].present?
         user_ids = params[:user_ids] if params[:user_ids].present?
-        product_ids = params[:product_ids] if params[:product_ids].present?
 
-        @shopcart.update(name: name, user_ids: user_ids, product_ids: product_ids)
+        @shopcart.update(name: name, user_ids: user_ids)
 
         render json: Api::V1::ShopcartSerializer.call(@shopcart), status: :ok
       end
